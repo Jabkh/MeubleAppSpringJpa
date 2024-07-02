10b4cc2115811ec08377e1779f38b68a05f4c810
Jaber kh <97162517+Jabkh@users.noreply.github.com>
2024-07-02 13:59:40 +0200

    CRUD géré cart géré Ajout des relations dans les entitées

---
+++ src/main/java/com/example/exercicespringjpa/ExerciceSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.exercicespringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciceSpringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciceSpringJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exercicespringjpa/ExerciceSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercicespringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciceSpringJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
